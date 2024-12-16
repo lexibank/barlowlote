e4b75580cb7c78c6f3f564b6c94699febd7561f0
chrzyki <christoph@functor-argument.net>
2024-12-16 09:07:20 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import json
+
+
+with open('metadata.json', encoding='utf-8') as fp:
+    metadata = json.load(fp)
+
+
+setup(
+    name='lexibank_barlowlote',
+    description=metadata['title'],
+    license=metadata.get('license', ''),
+    url=metadata.get('url', ''),
+    py_modules=['lexibank_barlowlote'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'lexibank.dataset': [
+            'barlowlote=lexibank_barlowlote:Dataset',
+        ]
+    },
+    install_requires=[
+        'pylexibank>=3.5.0',
+    ],
+    extras_require={
+        'test': [
+            'pytest-cldf',
+        ],
+    },
+)
